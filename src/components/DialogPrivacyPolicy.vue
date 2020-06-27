f38b65726def92b8aa29b3340e291967cc8f48ae
twknab <20636750+twknab@users.noreply.github.com>
2020-06-27 16:02:45 -0700

    update TOS and PP lang

--- src/components/DialogPrivacyPolicy.vue
+++ src/components/DialogPrivacyPolicy.vue
@@ -1,14 +1,114 @@
 <template>
   <div>
-    <mu-dialog title="GearList Privacy Policy" width="360" :open.sync="isOpen">
+    <mu-dialog
+      title="GearList Privacy Policy"
+      width="50%"
+      scrollable="true"
+      :open.sync="isOpen"
+    >
+      <h5>Effective June 27, 2020</h5>
+      <p>
+        GearList is operated by Sasquat.ch (“us”, “we”, or “our”) also known as,
+        <a href="http://sasquat.ch" target="_blank">http://sasquat.ch</a>
+        (Site). This page informs you of our policies regarding the collection,
+        use and disclosure of Personal Information we receive from users of the
+        Site.
+      </p>
+      <p>
+        Your privacy is very important to us. We use your Personal Information
+        only for providing and improving the Site. By using the Site, you agree
+        to the collection and use of information in accordance with this policy.
+      </p>
       <h3>What We Collect</h3>
-      <p>Lorem ipsum</p>
+      <h4>Personal Information</h4>
+      <p>
+        While using our Site, we may ask you to provide us with certain
+        personally identifiable information that can be used to contact or
+        identify you. Personally identifiable information may include, but is
+        not limited to, your name and your email address (“Personal
+        Information”).
+      </p>
+      <h4>Log Data</h4>
+      <p>
+        We collect information that your browser sends whenever you visit our
+        Site (“Log Data”). This Log Data may include information such as your
+        computer’s Internet Protocol (“IP”) address, browser type, browser
+        version, the pages of our Site that you visit, the time and date of your
+        visit, the time spent on those pages and other statistics.
+      </p>
+      <h4>Cookies</h4>
+      <p>
+        Cookies are files with small amount of data, which may include an
+        anonymous unique identifier. Cookies are sent to your browser from a web
+        site and stored on your computer’s hard drive. Like many sites, we use
+        “cookies” to collect information. You can instruct your browser to
+        refuse all cookies or to indicate when a cookie is being sent. However,
+        if you do not accept cookies, you may not be able to use some portions
+        of our Site.
+      </p>
       <h3>What We do with Your Data</h3>
-      <p>Lorem ipsum</p>
+      <p>
+        We may use your Personal Information to contact you with newsletters,
+        marketing or promotional materials and other information that be
+        relevant. We may also utilize your Personal Information to contact you
+        in the event of privacy policy updates, terms of service updates, or
+        important or critical updates. Analytics data may be used to understand
+        website usage patterns.
+      </p>
       <h3>Third Party Services</h3>
-      <p>Lorem ipsum</p>
+      <p>
+        We may use third party services such as Google Analytics that collect,
+        monitor and analyze site usage data. We use
+        <a href="https://www.google.com/recaptcha/intro/v3.html" target="_blank"
+          >Google reCAPTCHA</a
+        >
+        to prevent spam user registrations.
+      </p>
+
+      <p>
+        By registering with our site, you also agree to Google's
+        <a href="https://policies.google.com/privacy?hl=en" target="_blank"
+          >Privacy Policy</a
+        >
+        and
+        <a href="https://policies.google.com/terms?hl=en" target="_blank">TOS</a
+        >.
+      </p>
       <h3>Request Data Deletion</h3>
-      <p>Lorem ipsum</p>
+      <p>
+        You may request data deletion by emailing us at support@sasqut.ch.
+        Please contact us from the same email that you created your account
+        with, so we may verify your user account and identity.
+      </p>
+
+      <!-- 
+      
+      Communications We may use your Personal Information to contact
+      you with newsletters, marketing or promotional materials and other
+      information that be relevant. We may also utilize your Personal
+      Information to contact you in the event of privacy policy updates, terms
+      of service updates, or important or critical updates. 
+      
+       Security
+      The security of your Personal Information is important to us, but remember
+      that no method of transmission over the Internet, or method of electronic
+      storage, is 100% secure. While we strive to use commercially acceptable
+      means to protect your Personal Information, we cannot guarantee its
+      absolute security. Changes To This Privacy Policy This Privacy Policy is
+      effective as of November 8th, 2017 and will remain in effect except with
+      respect to any changes in its provisions in the future, which will be in
+      effect immediately after being posted on this page. We reserve the right
+      to update or change our Privacy Policy at any time and you should check
+      this Privacy Policy periodically. Your continued use of the Service after
+      we post any modifications to the Privacy Policy on this page will
+      constitute your acknowledgment of the modifications and your consent to
+      abide and be bound by the modified Privacy Policy. If we make any material
+      changes to this Privacy Policy, we will notify you either through the
+      email address you have provided us, or by placing a notice on our primary
+      website and applications. For this additional reason, you should keep your
+      contact and profile information current. Contact Us If you have any
+      questions about this Privacy Policy, please contact us at
+      support@sasquat.ch. -->
       <mu-button slot="actions" flat color="primary" @click="closePP"
         >Close</mu-button
       >
--- src/components/DialogTOS.vue
+++ src/components/DialogTOS.vue
@@ -1,23 +1,249 @@
 <template>
   <div>
     <mu-dialog
-      title="GearList Terms of Service"
-      width="360"
+      title="GearList Terms of Service & Use"
+      width="50%"
+      scrollable="true"
       :open.sync="isOpen"
     >
+      <h5>Effective June 26, 2020</h5>
       <p>
         By registering with GearList and using this tool, you agree to the
         following:
       </p>
-      <h3>Thing 1</h3>
-      <p>Lorem ipsum</p>
-      <h3>Thing 2</h3>
-      <p>Lorem ipsum</p>
-      <h3>Personal Data</h3>
-      <p>Lorem ipsum</p>
+      <p>
+        Welcome to the <a href="http://sasquat.ch">Sasquat.ch</a> GearList
+        service (the “Service”). The following Terms of Use apply when you view
+        or use the Service via our website located at
+        http://gearlist.sasquat.ch, http://www.gearlist.sasquat.ch or
+        https://gearlist.hikingtools.com.
+      </p>
+      <p>
+        Please review the following terms carefully. By accessing or using the
+        Service, you signify your agreement to these Terms of Use. If you do not
+        agree to these Terms of Use, you may not access or use the Service.
+        Privacy Policy The Company respects the privacy of its Service users.
+        Please refer to the Company's Privacy Policy (found here:
+        <a href="http://sasquat.ch/privacy">http://sasquat.ch/privacy</a>) which
+        explains how we collect, use, and disclose information that pertains to
+        your privacy. When you access or use the Service, you signify your
+        agreement to this Privacy Policy.
+      </p>
+      <h4>About The Service</h4>
+      <p>
+        The Service allows you to create a User account, and to then create Gear
+        Item(s) and Gear List(s), belonging to that User. Users may edit and
+        delete Items or Lists. Users cannot delete their own account at this
+        time but may contact us directly to request so at support@sasquat.ch.
+      </p>
+
+      <p></p>
+      <h4>Registration</h4>
+      <h5>Rules for User Conduct and Use of Service</h5>
+      <p>
+        You need to be at least 13 years old and a resident of the United States
+        to register for and use the Service. If you are a user who signs up for
+        the Service, will create a personalized account which includes a unique
+        user email and a password to access the Service and to receive messages
+        from the Company. We use
+        <a href="https://www.google.com/recaptcha/intro/v3.html" target="_blank"
+          >Google reCAPTCHA</a
+        >
+        to prevent spam user registrations. By registering with our site, you
+        also agree to Google's
+        <a href="https://policies.google.com/privacy?hl=en" target="_blank"
+          >Privacy Policy</a
+        >
+        and
+        <a href="https://policies.google.com/terms?hl=en" target="_blank">TOS</a
+        >. You agree to notify us immediately of any unauthorized use of your
+        password and/or account. The Company will not be responsible for any
+        liabilities, losses, or damages arising out of the unauthorized use of
+        your user account, password and/or account.
+      </p>
+      <h4>Restrictions</h4>
+      <p>
+        Permission to use the Site is conditioned upon the following Use
+        Restrictions and Conduct Restrictions: Use the service for any unlawful
+        purpose or for the promotion of illegal activities; Interfere or attempt
+        to interfere with the proper functioning of the Service; Make any
+        automated use of the system, or take any action that we deem to impose
+        or to potentially impose an unreasonable or disproportionately large
+        load on our servers or network infrastructure; Bypass any robot
+        exclusion headers or other measures we take to restrict access to the
+        Service or use any software, technology, or device to scrape, spider, or
+        crawl the Service or harvest or manipulate data; or Publish or link to
+        malicious content intended to damage or disrupt another user's browser,
+        computer, or network. Unauthorized use may result in criminal and/or
+        civil prosecution under Federal, State and local law. If you become
+        aware of misuse of our Service, please contact us support@sasquat.ch.
+      </p>
+
+      <h4>
+        Links to Other Sites and/or Materials
+      </h4>
+      <p>
+        As part of the Service, the Company may provide you with convenient
+        links to third party website(s) (“Third Party Sites”) as well as content
+        or items belonging to or originating from third parties (the “Third
+        Party Applications, Software or Content”). These links are provided as a
+        courtesy to Service subscribers. The Company has no control over Third
+        Party Sites and Third Party Applications, Software or Content or the
+        promotions, materials, information, goods or services available on these
+        Third Party Sites or Third Party Applications, Software or Content. Such
+        Third Party Sites and Third Party Applications, Software or Content are
+        not investigated, monitored or checked for accuracy, appropriateness, or
+        completeness by the Company, and the Company is not responsible for any
+        Third Party Sites accessed through the Site or any Third Party
+        Applications, Software or Content posted on, available through or
+        installed from the Site, including the content, accuracy, offensiveness,
+        opinions, reliability, privacy practices or other policies of or
+        contained in the Third Party Sites or the Third Party Applications,
+        Software or Content. Inclusion of, linking to or permitting the use or
+        installation of any Third Party Site or any Third Party Applications,
+        Software or Content does not imply approval or endorsement thereof by
+        the Company. If you decide to leave the Site and access the Third Party
+        Sites or to use or install any Third Party Applications, Software or
+        Content, you do so at your own risk and you should be aware that our
+        terms and policies no longer govern. You should review the applicable
+        terms and policies, including privacy and data gathering practices, of
+        any site to which you navigate from the Site or relating to any
+        applications you use or install from the site.
+      </p>
+
+      <h4>Copyright Complaints and Copyright Agent</h4>
+      <p>
+        The Company respects the intellectual property rights of others and
+        requests that the users do the same. If you are a copyright owner or an
+        agent thereof and believe, in good faith, that any materials provided on
+        the Service infringe upon your copyrights, you may submit a notification
+        to support@sasquat.ch. Please provide an electronic signature of a
+        person authorized to act on behalf of the owner of an exclusive right
+        that is allegedly infringed; A description of the copyrighted work
+        claimed to have been infringed, or, if multiple copyrighted works at a
+        single online site are covered by a single notification, a
+        representative list of such works at that site; A description of the
+        material that is claimed to be infringing or to be the subject of
+        infringing activity and information sufficient to enable us to locate
+        such work; Information reasonably sufficient to permit the service
+        provider to contact you, such as an address, telephone number, and/or
+        email address; A statement that you have a good faith belief that use of
+        the material in the manner complained of is not authorized by the
+        copyright owner, its agent, or the law; and A statement that the
+        information in the notification is accurate, and under penalty of
+        perjury, that you are authorized to act on behalf of the owner of an
+        exclusive right that is allegedly infringed. We reserve all rights that
+        are not expressly granted to you under this Agreement.
+      </p>
+
+      <h4>User Consent to Receive Communciations in Electronic Form</h4>
+      <p>
+        For contractual purposes, you (a) consent to receive communications from
+        the Company in an electronic form via the email address you have
+        submitted; and (b) agree that all Terms of Use, agreements, notices,
+        disclosures, and other communications that the Company provides to you
+        electronically satisfy any legal requirement that such communications
+        would satisfy if it were in writing. The foregoing does not affect your
+        non-waivable rights. We may also use your email address, to send you
+        other messages, including information about the Company and special
+        offers. You may opt out of such email by changing your account settings
+        or sending an email to support@sasquat.ch . Opting out may prevent you
+        from receiving messages regarding the Company or special offers.
+      </p>
+
+      <h4>Warranty Disclaimer</h4>
+      <p>
+        THE SERVICE, IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND. WITHOUT
+        LIMITING THE FOREGOING, THE COMPANY EXPRESSLY DISCLAIMS ALL WARRANTIES,
+        WHETHER EXPRESS, IMPLIED OR STATUTORY, REGARDING THE SERVICE INCLUDING
+        WITHOUT LIMITATION ANY WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+        PARTICULAR PURPOSE, TITLE, SECURITY, ACCURACY AND NON-INFRINGEMENT.
+        WITHOUT LIMITING THE FOREGOING, THE COMPANY MAKES NO WARRANTY OR
+        REPRESENTATION THAT ACCESS TO OR OPERATION OF THE SERVICE WILL BE
+        UNINTERRUPTED OR ERROR FREE. YOU ASSUME FULL RESPONSIBILITY AND RISK OF
+        LOSS RESULTING FROM YOUR DOWNLOADING AND/OR USE OF FILES, INFORMATION,
+        CONTENT OR OTHER MATERIAL OBTAINED FROM THE SERVICE. SOME JURISDICTIONS
+        LIMIT OR DO NOT PERMIT DISCLAIMERS OF WARRANTY, SO THIS PROVISION MAY
+        NOT APPLY TO YOU. Limitation of Damages; Release TO THE EXTENT PERMITTED
+        BY APPLICABLE LAW, IN NO EVENT SHALL THE COMPANY, ITS AFFILIATES,
+        DIRECTORS, OR EMPLOYEES, OR ITS LICENSORS OR PARTNERS, BE LIABLE TO YOU
+        FOR ANY LOSS OF PROFITS, USE, OR DATA, OR FOR ANY INCIDENTAL, INDIRECT,
+        SPECIAL, CONSEQUENTIAL OR EXEMPLARY DAMAGES, HOWEVER ARISING, THAT
+        RESULT FROM (A) THE USE, DISCLOSURE, OR DISPLAY OF YOUR USER CONTENT;
+        (B) YOUR USE OR INABILITY TO USE THE SERVICE; (C) THE SERVICE GENERALLY
+        OR THE SOFTWARE OR SYSTEMS THAT MAKE THE SERVICE AVAILABLE; OR (D) ANY
+        OTHER INTERACTIONS WITH THE COMPANY OR ANY OTHER USER OF THE SERVICE,
+        WHETHER BASED ON WARRANTY, CONTRACT, TORT (INCLUDING NEGLIGENCE) OR ANY
+        OTHER LEGAL THEORY, AND WHETHER OR NOT THE COMPANY HAS BEEN INFORMED OF
+        THE POSSIBILITY OF SUCH DAMAGE, AND EVEN IF A REMEDY SET FORTH HEREIN IS
+        FOUND TO HAVE FAILED OF ITS ESSENTIAL PURPOSE. SOME JURISDICTIONS LIMIT
+        OR DO NOT PERMIT DISCLAIMERS OF LIABILITY, SO THIS PROVISION MAY NOT
+        APPLY TO YOU.
+      </p>
+      <p>
+        If you have a dispute with one or more users, a restaurant or a merchant
+        of a product or service that you review using the Service, you release
+        us (and our officers, directors, agents, subsidiaries, joint ventures
+        and employees) from claims, demands and damages (actual and
+        consequential) of every kind and nature, known and unknown, arising out
+        of or in any way connected with such disputes. If you are a California
+        resident, you waive California Civil Code §1542, which says: “A general
+        release does not extend to claims which the creditor does not know or
+        suspect to exist in his favor at the time of executing the release,
+        which if known by him must have materially affected his settlement with
+        the debtor.” Modification of Terms of Use We can amend these Terms of
+        Use at any time and will update these Terms of Use in the event of any
+        such amendments. It is your sole responsibility to check the Site from
+        time to time to view any such changes in the Agreement. If you continue
+        to use the Site, you signify your agreement to our revisions to these
+        Terms of Use. However, we will notify you of material chances to the
+        terms by posting a notice on our homepage and/or sending an email to the
+        email address you provided to us upon registration. For this additional
+        reason, you should keep your contact and profile information current.
+        Any changes to these Terms (other than as set forth in this paragraph)
+        or waiver of the Company's rights hereunder shall not be valid or
+        effective except in a written agreement bearing the physical signature
+        of an officer of the Company. No purported waiver or modification of
+        this Agreement by the Company via telephonic or email communications
+        shall be valid.
+      </p>
+
+      <h4>General Terms</h4>
+      <p>
+        If any part of this Agreement is held invalid or unenforceable, that
+        portion of the Agreement will be construed consistent with applicable
+        law. The remaining portions will remain in full force and effect. Any
+        failure on the part of the Company to enforce any provision of this
+        Agreement will not be considered a waiver of our right to enforce such
+        provision. Our rights under this Agreement will survive any termination
+        of this Agreement. You agree that any cause of action related to or
+        arising out of your relationship with the Company must commence within
+        ONE year after the cause of action accrues. Otherwise, such cause of
+        action is permanently barred. These Terms of Use and your use of the
+        Site are governed by the federal laws of the United States of America
+        and the laws of the State of Washington, without regard to conflict of
+        law provisions. The Company may assign or delegate these Terms of
+        Service and/or the Company's Privacy Policy, in whole or in part, to any
+        person or entity at any time with or without your consent. You may not
+        assign or delegate any rights or obligations under the Terms of Service
+        or Privacy Policy without the Company's prior written consent, and any
+        unauthorized assignment and delegation by you is void.
+      </p>
+
+      <p>
+        YOU ACKNOWLEDGE THAT YOU HAVE READ THESE TERMS OF USE, UNDERSTAND THE
+        TERMS OF USE, AND WILL BE BOUND BY THESE TERMS AND CONDITIONS. YOU
+        FURTHER ACKNOWLEDGE THAT THESE TERMS OF USE TOGETHER WITH THE PRIVACY
+        POLICY AT
+        <a href="http://sasquat.ch/privacy">HTTP://SASQUAT.CH/PRIVACY</a>
+        REPRESENT THE COMPLETE AND EXCLUSIVE STATEMENT OF THE AGREEMENT BETWEEN
+        US AND THAT IT SUPERSEDES ANY PROPOSAL OR PRIOR AGREEMENT ORAL OR
+        WRITTEN, AND ANY OTHER COMMUNICATIONS BETWEEN US RELATING TO THE SUBJECT
+        MATTER OF THIS AGREEMENT.
+      </p>
       <mu-button slot="actions" flat color="primary" @click="closeTOS"
-        >Close</mu-button
-      >
+        >Close
+      </mu-button>
     </mu-dialog>
   </div>
 </template>
